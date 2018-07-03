7c4852f7f4e4d49ed953b2165df4243e5cccbfbc
rudyramdat <rudyramdat1@gmail.com>
2018-07-03 15:40:00 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,18 @@
+       $(document).ready(function() {
+
+              /*this was a test*/
+
+        /// $('.info').css({color:'green',fontStyle:'italic'});
+
+
+$('.info').mouseleave(function(){
+      $('.info').fadeTo('fast', 0.4);
+      
+});
+$('.info').mouseenter(function(){
+    $('.info').fadeTo('fast',1);  
+});
+       });
+
+
+           
